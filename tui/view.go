519ce1cb1f652ddcec971a14c25f18dd3becdfc1
Adam P <adam.porbanderwalla@gmail.com>
2026-02-26 15:26:14 -0500

    feat: added basic visual layer handling for tui

--- tui/view.go
+++ tui/view.go
@@ -1,11 +1,21 @@
 package tui
 
 import(
-	"theia/filesystem"
-	tea "github.com/charmbracelet/bubbletea"
+	"fmt"
+	"strings"
 )
 
 func (m Model) View() string{
-	return ""
+	var s strings.Builder
+	s.WriteString(fmt.Sprintf("Exploring: %s\n", m.Path))
+	for i, file := range m.SystemFiles{
+		cursor := " "
+		if m.Cursor == i{
+			cursor = ">"
+		}
+		s.WriteString(fmt.Sprintf("%s %s\n", cursor, file.Name))
+	}
+	s.WriteString("\n [tab] enter  [backspace] back  [enter] select  [q] quit\n")
+	return s.String()
 }
 

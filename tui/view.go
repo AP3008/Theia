de166ee46d4af8c731fa951857a5e1d829836a21
Adam P <adam.porbanderwalla@gmail.com>
2026-02-26 17:40:08 -0500

    feat: functional -l flag

--- tui/view.go
+++ tui/view.go
@@ -9,9 +9,11 @@ func (m Model) View() string{
 	var str string
 	end := m.TopRow + m.Height
 	end = min(end, len(m.SystemFiles))
-	if !m.Settings.ShowDetails{
+	if m.Settings.ShowDetails{
+		str = longView(&m, end)
+	} else {
 		str = normalView(&m, end)
-	}
+	} 
 	return str
 }
 
@@ -31,9 +33,36 @@ func normalView(m *Model, end int) string{
 	return s.String()
 }
 
+// Handles -l flag 
 func longView(m *Model, end int)string{
 	var s strings.Builder
 	s.WriteString(fmt.Sprintf("Exlporing: %s\n", m.Path))
+	
+	// Header
+	s.WriteString(fmt.Sprintf("  %-10s %-8s %-12s %s\n", "MODE", "SIZE", "MODIFIED", "NAME"))
+    s.WriteString(strings.Repeat("-", 50) + "\n")
 
+	visibleFiles := m.SystemFiles[m.TopRow:end]
+
+	for i, file := range visibleFiles{
+		actualIndex := i + m.TopRow
+		cursor := " "
+		if m.Cursor == actualIndex{
+			cursor = ">"
+		}
+
+		sizeStr := fmt.Sprintf("%d B", file.Size)
+		if file.IsDir{
+			sizeStr = "-"
+		}
+		line := fmt.Sprintf("%s %-10s %8s %-12s %s\n",
+            cursor,
+            file.Permission.String(), 
+            sizeStr,
+            file.ModifiedTime.Format("Jan 02 15:04"), 
+            file.Name,
+        )
+        s.WriteString(line)
+	}
 	return s.String()
 }

d26be5e2c561e7c7de86061355aafe4c0000d2b1
Adam P <adam.porbanderwalla@gmail.com>
2026-02-26 14:13:50 -0500

    feat: created Model for bubbletea & init state

--- tui/model.go
+++ tui/model.go
@@ -1,6 +1,39 @@
+// Contains the state that bubble tea requires that will define Theia's TUI
 package tui
 
 import (
-	"os"
 	"theia/filesystem"
+	tea "github.com/charmbracelet/bubbletea"
 )
+
+type Model struct {
+	Path string 
+	SystemFiles []filesystem.SystemFile
+	Cursor int
+	Selected string
+	ShowDetails bool
+}
+
+func (m Model) Init() tea.Cmd{
+	return nil
+}
+
+// Declares the initial model state
+
+func InitialModel(path string, details bool) (Model, error){
+	fs_list, err := filesystem.CreateSystemFileList(path)
+	if err != nil{
+		return Model{}, err
+	}
+	var selected_str string
+	if len(fs_list) > 0 {
+		selected_str = fs_list[0].Path
+	}
+	return Model {
+		Path : path,
+		SystemFiles : fs_list,
+		Cursor : 0,
+		Selected : selected_str,
+		ShowDetails: details,
+	}, nil
+}

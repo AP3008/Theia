9905151669c7507cd64d2c87b8e4a7c3c3d6954f
Adam P <adam.porbanderwalla@gmail.com>
2026-02-25 20:44:11 -0500

    feat: added private helper to sort the list

--- filesystem/scanner.go
+++ filesystem/scanner.go
@@ -4,6 +4,7 @@ package filesystem
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 // We Create a slice of SystemFiles to get a directory in a consumeable format
@@ -20,5 +21,23 @@ func CreateSystemFileList (path string) ([]SystemFile, error){
 		}
 		directoryList = append(directoryList, sf)
 	}
+	sortDirList(directoryList)
 	return directoryList, nil
+}
+
+// Private helper to help sort the dir list
+// Because we are using slices we don't actually need to pass a reference
+func sortDirList(dl []SystemFile){
+	n := len(dl)
+	for i := 0; i < n-1; i++{
+		for j:=0; j < n-i; j++{
+			name1 := strings.ToLower(dl[j].Name)
+			name2 := strings.ToLower(dl[j+1].Name)
+			if name1 < name2{
+				tmp := dl[j]
+				dl[j] = dl[j+1]
+				dl[j+1] = tmp
+			}
+		}
+	}
 }

edc6683e9db814213baf03aa203905469979a1d4
Adam P <adam.porbanderwalla@gmail.com>
2026-02-27 15:47:36 -0500

    feat: added more colours & styling for perms

--- tui/view.go
+++ tui/view.go
@@ -7,7 +7,7 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-// Lipgloss defined styles 
+// Lipgloss defined styles for general use 
 var (
 	// header styling 
 	headerStyle = lipgloss.NewStyle().
@@ -15,6 +15,88 @@ var (
 		Foreground(lipgloss.Color("#FAFAFA")).
 		//Padding(0, 1).
 		MarginBottom(1)
+	
+	// cursor style
+	cursorStyle = lipgloss.NewStyle().
+        Foreground(lipgloss.Color("#EE6FF8")).
+        Bold(true)
+	
+	// Directory Names
+	dirStyle = lipgloss.NewStyle().
+        Foreground(lipgloss.Color("#61AFEF"))
+	
+	// Symlink Names
+	symlinkStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#56B6C2"))
+	
+	// Executable name
+	
+	execStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#98C379"))
+
+	// Regular File name
+	
+	regStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ABB2BF"))
+
+	// Design for colour button info
+	infoStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#82827E"))
+)
+
+func (m Model) View() string{
+	var str string
+	end := m.TopRow + m.Height
+	end = min(end, len(m.SystemFiles))
+	if m.Settings.ShowDetails{
+		str = longView(&m, end)
+	} else {
+		str = normalView(&m, end)
+	} 
+	return str
+}
+
+func normalView(m *Model, end int) string{
+	var s strings.Builder
+	visibleFiles := m.SystemFiles[m.TopRow:end]
+	header := headerStyle.Render(fmt.Sprintf("Exploring: " + tildaPath(m.Path)))
+	s.WriteString(header + "\n")
+	for i, file := range visibleFiles{
+		actualIndex := i + m.TopRow
+		cursor := " "
+		if m.Cursor == actualIndex{
+			cursor = cursorStyle.Render("> ")
+		}
+		name := regStyle.Render(file.Name)
+		if file.IsDir{
+			name = dirStyle.Render(file.Name + "/")
+		}
+		if file.IsSymLink{
+			name = symlinkStyle.Render(name) 
+		}
+		s.WriteString(fmt.Sprintf("%s %s\n", cursor, name))
+	}
+	info := infoStyle.Render("\n [tab] enter directory [backspace] parent directory [enter] select  [q] quit [ctrl+o] return this directory\n")
+	s.WriteString(info)
+	return s.String()
+}
+package tui
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// Lipgloss defined styles 
+var (
+	// header styling 
+	headerStyle = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("#FAFAFA"))
+		//Padding(0, 1).
+		//MarginBottom(1)
 	
 	// cursor style
 	cursorStyle = lipgloss.NewStyle().
@@ -65,8 +147,8 @@ func (m Model) View() string{
 func normalView(m *Model, end int) string{
 	var s strings.Builder
 	visibleFiles := m.SystemFiles[m.TopRow:end]
-	header := headerStyle.Render(fmt.Sprintf("Exploring: " + tildaPath(m.Path)))
-	s.WriteString(header + "\n")
+	header := headerStyle.Render(fmt.Sprintf("Exploring: %s\n\n", tildaPath(m.Path)))
+	s.WriteString(header)
 	for i, file := range visibleFiles{
 		actualIndex := i + m.TopRow
 		cursor := " "
@@ -167,4 +249,135 @@ func tildaPath(path string) string{
 		return strings.Replace(path, home, "~", 1)
 	}
 	return path
+}
+
+// Styling for longview 
+var (
+	// Column headings
+	columnHeaderStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#5C6370")).
+		Underline(true).
+		Bold(true)
+	
+	// Rose Pine colours
+	rose    = lipgloss.Color("#ebbcba")
+	gold    = lipgloss.Color("#f6c177")
+	iris    = lipgloss.Color("#c4a7e7")
+	pine    = lipgloss.Color("#31748f")
+	love    = lipgloss.Color("#eb6f92")
+	muted   = lipgloss.Color("#6e6a86")
+
+	// Style Objects
+	sizeStyle = lipgloss.NewStyle().Foreground(rose)
+	dateStyle = lipgloss.NewStyle().Foreground(gold)
+)
+
+// Dimensions 
+const (
+    modeW = 12
+    sizeW = 10
+    modW  = 16
+)
+// Handles -l flag 
+func longView(m *Model, end int)string{
+	var s strings.Builder
+	s.WriteString(fmt.Sprintf("Exlporing: %s\n\n", tildaPath(m.Path)))
+	
+	// Header
+	//Escape Code for underlined 
+	u := "\033[4m"
+	r := "\033[0m"
+
+	// Column widths
+	const (
+		modeW = 10
+		sizeW = 8
+		modW  = 12
+	)
+
+	s.WriteString("  ") 
+	
+	s.WriteString(fmt.Sprintf("%s%s%s%*s ", u, "MODE", r, modeW-len("MODE"), ""))
+	
+	s.WriteString(fmt.Sprintf("%s%s%s%*s ", u, "SIZE", r, sizeW-len("SIZE"), ""))
+	
+	s.WriteString(fmt.Sprintf("%s%s%s%*s ", u, "MODIFIED", r, modW-len("MODIFIED"), ""))
+	
+	s.WriteString(fmt.Sprintf("%s%s%s\n", u, "NAME", r))
+
+	visibleFiles := m.SystemFiles[m.TopRow:end]
+
+	for i, file := range visibleFiles{
+		actualIndex := i + m.TopRow
+		cursor := " "
+		if m.Cursor == actualIndex{
+			cursor = ">"
+		}
+
+		sizeStr := "-"
+		if !file.IsDir{
+			sizeStr = formatBytes(file.Size)
+		}
+		line := fmt.Sprintf("%s %-10s %8s %-12s %s\n",
+            cursor,
+            file.Permission.String(), 
+            sizeStr,
+            file.ModifiedTime.Format("Jan 02 15:04"), 
+    	        file.Name,
+        )
+        s.WriteString(line)
+	}
+	info := infoStyle.Render("\n [tab] enter directory [backspace] parent directory [enter] select  [q] quit [ctrl+o] return this directory\n")
+	s.WriteString(info)
+	return s.String()
+}
+
+// Private helper to format bytes
+
+func formatBytes(bytes int64) string{
+	const unit = 1024
+	if bytes < unit {
+		return fmt.Sprintf("%d B", bytes)
+	}
+	divisor := int64(unit)
+	exponent := 0
+	for n := bytes/unit; n>=unit; n/=unit{
+		divisor *= unit
+		exponent++
+	}
+	return fmt.Sprintf("%.1f %cB",float64(bytes)/float64(divisor), "KMGT"[exponent])
+}
+
+// Private helper to remove home dir
+
+func tildaPath(path string) string{
+	home, err := os.UserHomeDir()
+	if err != nil{
+		return path 
+	}
+	if strings.HasPrefix(path, home){
+		return strings.Replace(path, home, "~", 1)
+	}
+	return path
+}
+
+// Private helper to style the permissions
+
+func permStyler(perm string) string{
+	var s strings.Builder
+	for _, c := range perm{
+		switch c{
+		case "r":
+			s.WriteString(lipgloss.NewStyle().Foreground(iris).Render(c))
+		case "w":
+			s.WriteString(lipgloss.NewStyle().Foreground(pine).Render(c))
+		case "x":
+			s.WriteString(lipgloss.NewStyle().Foreground(love).Render(c))
+		case"d"|"l":
+			s.WriteString(lipgloss.NewStyle().Foreground(love).Render(c))
+		default:
+			s.WriteString(lipgloss.NewStyle().Foreground(muted).Render(c))
+		}
+	}
+	return s.String()
 }
